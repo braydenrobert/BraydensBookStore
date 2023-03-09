86c4464c92ad37ed7d510e6dd0677aeb1629bd0b
braydenrobert <W0740867@myscc.ca>
2023-03-09 15:18:35 -0500

    -Stopped errors from occuring

--- BraydensBookStore/Startup.cs
+++ BraydensBookStore/Startup.cs
@@ -1,4 +1,4 @@
-using BraydensBookStore.Data;
+//using BraydensBookStore.DataAccess;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +25,7 @@ namespace BraydensBookStore
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /*
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -35,6 +36,7 @@ namespace BraydensBookStore
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
+        */
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
